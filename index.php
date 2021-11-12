76f46084f573e40909f93c79cc829cc0f82afb54
Aelie54 <klein.amelie1991@gmail.com>
2021-11-12 14:17:56 +0100

    commit on 'page html'

--- index.php
+++ index.php
@@ -1,1 +1,48 @@
-hello
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- En-tête de la page -->
+        <meta charset="utf-8" />
+        <title>Practive Time</title>
+    </head>
+
+    <body>
+
+    <form action="/ma-page-de-traitement" method="post">
+    <div>
+        <label for="Date AAAA-MM-NN">Nom :</label>
+        <input type="date" id="date" name="dateuser_name">
+    </div>
+    <div>
+    <SELECT for="selectionner exercice" name="exercise" size="1">
+        <OPTION>Developpé couché
+        <OPTION>soulevé de terre
+        <OPTION>traction
+        <OPTION>soulevé militaire
+        <OPTION>squat
+        <OPTION>fente
+        <OPTION>corde traction triceps
+        <OPTION>tirage poitrine
+        <OPTION>extension lombaire
+        <OPTION>Abdos sur banc : levé de jambes
+        <OPTION>Abdos sur banc : levé de buste
+        <OPTION>Tapis de course
+        <OPTION>Vélo
+        <OPTION>Monte marches
+        <OPTION>Vélo élliptique
+        <OPTION>Planche latérale gauche
+        <OPTION>Planche latérale droite
+        <OPTION>Planche face au sol sur coude
+        <OPTION>Planche face au sol bras tendus
+        <OPTION>Pont bras au sol
+        <OPTION>Pont bras tendus
+</SELECT>
+    </div>
+    <div>
+        <label for="msg">Message :</label>
+        <textarea id="msg" name="user_message"></textarea>
+    </div>
+</form>
+
+    </body>
+</html>
