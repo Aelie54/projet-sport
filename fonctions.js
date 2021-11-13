55ef3b2b9d7021999f976a6777a2f70d9e1dbe0f
Amélie Klein <klein.amelie1991@gmail.com>
2021-11-13 20:56:14 +0100

    front ok

--- fonctions.js
+++ fonctions.js
@@ -1,10 +1,351 @@
-var choice = document.getElementById("select")
+function choose() {
 
+    switch (document.getElementById("mychoice").selectedIndex) {
 
-    if (choice.selectIndex == 0){
+    case (select = 0):
+    //document.getElementById("flexbox").style.display = "none";
+    document.getElementById("poids").style.display = "none";
+    document.getElementById("nombre").style.display = "none";
+    document.getElementById("degrès").style.display = "none";
+    document.getElementById("minutes").style.display = "none";
+    document.getElementById("vitesse").style.display = "none";
+    document.getElementById("difficulte").style.display = "none";
+    document.getElementById("distance").style.display = "none";
+    document.getElementById("marches").style.display = "none";
+    document.getElementById("active").style.display = "none";
+    document.getElementById("image").style.display = "block";
+    document.getElementById("image2").src="https://cdn.pixabay.com/photo/2016/06/03/08/18/oops-1432954_960_720.png";
+    document.getElementById("valider").style.display = "none";
+    break;
 
-    alert("fhh");
+    case (select =1):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "none";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/1.gif";
+    break;
+
+    case (select =2):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "none";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/2.gif";
+    break;
+
+    case (select =3):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "none";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/3.jpeg";
+    break;
+
+    case (select =4):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "none";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/4.gif";
+    break;
+
+    case (select =5):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "none";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/5.gif";
+    break;
+
+    case (select =6):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "none";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/6.gif";
+    break;
+
+    case (select =7):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "none";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/7.gif";
+    break;
+
+    case (select =8):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "none";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/8.gif";
+    break;
+
+    case (select =9):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "none";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/9.gif";
+    break;
+
+    case (select = 10): //levee de jambes
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "none";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "block"
+        document.getElementById("minutes").style.display = "none";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "none";
+                document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/10.jpg";
+    break;
+
+    case (select = 11): //levee de buste
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "block"
+        document.getElementById("minutes").style.display = "none";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/11.jpg";
+    break;
+
+    case (select =16):        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "block";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "block";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/16-17.png";
+        break;
+
+    case (select =17):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "block";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "block";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/16-17.png";
+        break;
+
+    case (select =18):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "block";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "block";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/18.png";
+        break;
+
+    case (select =19):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "block";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "block";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/19.jpg";
+        break;
+
+    case (select =20):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "block";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "block";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/20.png";
+        break;
+
+    case (select =21):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "block";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "block";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/21.jpg";
+        break;
+
+    case (select =22):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "block";
+        document.getElementById("nombre").style.display = "block";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "block";
+        document.getElementById("vitesse").style.display = "none";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "none";
+        document.getElementById("active").style.display = "block";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/22.jpg";
+        break;
+    
+    case (select =12):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "none";
+        document.getElementById("nombre").style.display = "none";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "block";
+        document.getElementById("vitesse").style.display = "block";
+        document.getElementById("difficulte").style.display = "block";
+        document.getElementById("distance").style.display = "block";
+        document.getElementById("marches").style.display = "block";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/12.jpg";
+    break;
+
+    case (select =13):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "none";
+        document.getElementById("nombre").style.display = "none";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "block";
+        document.getElementById("vitesse").style.display = "block";
+        document.getElementById("difficulte").style.display = "block";
+        document.getElementById("distance").style.display = "block";
+        document.getElementById("marches").style.display = "block";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/13.jpg";
+    break;
+
+    case (select =15):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "none";
+        document.getElementById("nombre").style.display = "none";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "block";
+        document.getElementById("vitesse").style.display = "block";
+        document.getElementById("difficulte").style.display = "block";
+        document.getElementById("distance").style.display = "block";
+        document.getElementById("marches").style.display = "block";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/15.jpg";
+    break;
+
+    case (select =14):
+        document.getElementById("flexbox").style.display = "flex";
+        document.getElementById("poids").style.display = "none";
+        document.getElementById("nombre").style.display = "none";
+        document.getElementById("degrès").style.display = "none";
+        document.getElementById("minutes").style.display = "block";
+        document.getElementById("vitesse").style.display = "block";
+        document.getElementById("difficulte").style.display = "none";
+        document.getElementById("distance").style.display = "none";
+        document.getElementById("marches").style.display = "block";
+        document.getElementById("active").style.display = "none";
+        document.getElementById("image").style.display = "block";
+        document.getElementById("image2").src="./images/14.jpg";
+    break;
+    }
 
 }
-
-
--- index.php
+++ index.php
@@ -1,42 +1,41 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <!-- En-tête de la page -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width">   
         <link rel="stylesheet" href="style.css" />
-        <script type="text/javascript" src="functions.js"></script>
+        <script type="text/javascript" src="fonctions.js"></script>
         <title>Practive Time</title>
 
     </head>
 
-
     <body>
 
+<!-- deux boutons du haut-->
 <div class="boutons_nav">
+    <br><br>
     <input class="Mes_entraînements"
        type="button" onclick="window.location.href = 'mes_entrainements.php';"
        value="Mes entraînements">
-
     <input class="Mon_évolution"
        type="button" onclick="window.location.href = 'mon_evolution.php';"
        value="Mon évolution">
+    <br><br>
 </div>
 
 <br><br><br>
 
+<!-- entrer date et choix exercice-->
 <div class="exercices_choice">
     <form method="post" required>
-        <div>
+        <div><br>
             <label for="Date AAAA-MM-NN">Nom :</label>
             <input type="date" id="date" name="dateuser_name">
         </div><br>
 
-        <select for="selectionner exercice" name="select" id= "select" required>
+        <select for="selectionner exercice" name="select" id= "mychoice">
             <option value="selectionner_exercice">selectionner exercice</option>
-            <!-- Index = 0
-            Valeur = selectionner_exercice
-            Texte = selectionner exercice -->
+            <!-- Index = 0 et Valeur = selectionner_exercice et Texte = selectionner exercice -->
             <option value="developpe_couche">Developpé couché</option>
             <option value="souleve_de_terre">Soulevé de terre</option>
             <option value="traction">Traction</option>
@@ -56,73 +55,86 @@
             <option value="pld">Planche latérale droite</option>
             <option value="psc">Planche face au sol sur coude</option>
             <option value="psb">Planche face au sol bras tendus</option>
-            <option value="pont_sol">Pont bras au sol</option>
-            <option value="pont_bras">Pont bras tendus</option>
+            <option value="pont_sol">Pont bras bras tendus</option>
+            <option value="pont_bras">Pont bras tendus jambes pliées</option>
+            <option value="pont_bras">Pont épaules au sol</option>
         </select><br><br>
         
-        <button type="button" onclick="choose()" id="button">Sélectionné</button>
+        <button type="button" onclick="choose()" id="bouton_selection">Sélectionner</button>
+    <br><br>
     </form>
 </div>
 
-<br>
-<br>
-<img class="image" src="https://www.sport-passion.fr/pictures/alteres.jpg" />
-<br>
-<br>
 
+
+<!-- afficher image exercice et champs apramètres à remplir-->
 <div class="afficher_exercice" id="flexbox">
-    <form for="/truc" method="post">
-        <div>
+
+    <form for="paramètres" method="post">
+        <div id="image"><br>
+            <img  id="image2" src="https://www.sport-passion.fr/pictures/alteres.jpg" />
+            <br></div>
+
+        <div id="poids">
             <label for="Kg">Poids ajoutés :</label>
-            <input type="number" step="0.25" id="poids" name="dateuser_name">
+            <input type="number" step="0.25" id="poids" name="dateuser_name"> 
         </div>
-        <div>
+
+        <div id="nombre">
             <label for="Nombre entier">Nombre de levées :</label>
             <input type="number" id="levees" name="dateuser_name">
         </div>
-        <div>
+
+        <div id="degrès">
             <label for="Degrès">Pente ou inclinaison :</label>
             <input type="number" id="pente" name="dateuser_name">
         </div>
-        <div>
+
+        <div id="minutes">
             <label for="Minutes">Durée de l'exercice :</label>
             <input type="entier" id="minutes" name="dateuser_name">
         </div>
-        <div>
+
+        <div id="vitesse">
             <label for="Km/h">Vitesse de l'exercice :</label>
             <input type="number" step="0.25"  id="vitesse" name="dateuser_name">
         </div>
-        <div>
+
+        <div id="difficulte">
             <label for="Nombre entier">Difficulté choisie :</label>
             <input type="number" id="difficulte" name="dateuser_name">
         </div>
-        <div>
+
+        <div id="distance">
             <label for="Nombre entier">Distance réalisée :</label>
             <input type="number" step="0.25" id="difficulte" name="dateuser_name">
         </div>
-        <div>
+
+        <div id="marches">
             <label for="Nombre entier">Nombre de marches :</label>
             <input type="number" id="difficulte" name="dateuser_name">
         </div>
-        <div>
+
+        <div id="active">
             <label for="Oui ou Non">Active :</label>
-            <SELECT>
+            <select>
             <OPTION>Non
             <OPTION>Oui
-            </SELECT>
+            </select>
         </div>
+
     </form>
 
     <br><br>
 
-    <div class="boutons_valider">
+    <div class="bouton_valider" id="valider">
         <input class="Valider"
         type="button"
-        value="Valider">
+        value="Valider"
+        id="valider">
     </div>
-
+    <br>
 </div>
-
 
 <!-- https://enacit.epfl.ch/cours/html/exercices/ex_boutons.html*/ 
      https://cssdeck.com/labs/css3-flexbox-flexible-box-layouts-flex-website -->
